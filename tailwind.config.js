7fd77d58a53a4db6a8f3622a75ff797a44eac172
Rubenhdz20 <rubhernandez42@gmail.com>
2023-12-16 20:37:42 -0600

    Add Tailwind for the project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  './pages/**/*.{html,js}',
+  './components/**/*.{html,js}',
+  './index.html',
+	'./public/index.html', 
+	'./src/**/*.{html,js}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
