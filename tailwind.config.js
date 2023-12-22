34c56f4410091f37cc911e4cd4849e7089207ca5
Rubenhdz20 <rubhernandez42@gmail.com>
2023-12-21 22:51:23 -0600

    Form edit with styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,11 @@ module.exports = {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
+      "custom-white": "#FFFFFF",
       "custom-gray": "#969696",
       "custom-black": "#15202A",
       "light-blue": "#C2D3FF",
+      "pale-blue": "hsl(223, 100%, 88%)",
       "strong-blue": "#4C7BF3"
     }
   },
