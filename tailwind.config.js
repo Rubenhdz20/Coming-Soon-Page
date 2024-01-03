8c3bfc5ec45811a59d5cfd203333ea9d9b5e8664
Rubenhdz20 <rubhernandez42@gmail.com>
2024-01-02 20:10:54 -0600

    Work in Dark Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,15 @@ module.exports = {
       current: 'currentColor',
       "custom-white": "#FFFFFF",
       "custom-gray": "#969696",
+      "custom-gray-strong": "#111827",
       "custom-black": "#15202A",
       "light-blue": "#C2D3FF",
       "pale-blue": "hsl(223, 100%, 88%)",
       "strong-blue": "#4C7BF3"
-    }
+    },
+    fontFamily: {
+      sans: ['Libre Franklin', 'sans-serif'],
+    },
   },
   plugins: [],
 }
