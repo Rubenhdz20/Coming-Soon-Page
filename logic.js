61a80ca43690b6138eb560a77fb0bd412221a8de
Rubenhdz20 <rubhernandez42@gmail.com>
2024-01-15 18:38:36 -0600

    Finally javsacript done and styles

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 const emailInput = document.querySelector('#email');
 const errorMessage = document.querySelector('#email-error');
 const notifyButton = document.querySelector('#notify-button');
+const thanksMessage = document.querySelector('#thankYouMessage');
   
 notifyButton.addEventListener('click', function() {
     event.preventDefault();
@@ -9,16 +10,23 @@ notifyButton.addEventListener('click', function() {
   
     if (!isValidEmail || emailInput.value === '') {
     // Display error message
-    errorMessage.classList.add("flex", "items-center", "bottom-6", "text-red-400");
+    errorMessage.classList.add("flex", "items-center", "bottom-6", "text-red-500");
     errorMessage.classList.remove("hidden");
+    thanksMessage.classList.add("hidden");
+    
   
     // Add error class to the input for styling
-    emailInput.classList.remove('border-light-blue', 'placeholder-light-blue', 'focus:border-strong-blue');
-    emailInput.classList.add('border-red-400', 'placeholder-red-400', 'focus:border-red-400');
+    emailInput.classList.remove('border-light-blue', 'placeholder-light-blue', 'focus:border-strong-blue', 'dark:border-gray-700');
+    emailInput.classList.add('border-red-500', 'placeholder-red-500', 'focus:border-red-500');
     } else {
     // Clear error message and styling if the email is valid
     errorMessage.classList.add('hidden');
-    emailInput.classList.remove('error');
+    emailInput.classList.remove('border-red-500', 'placeholder-red-500', 'focus:border-red-500');
+    emailInput.classList.add('border-light-blue', 'placeholder-light-blue', 'focus:border-strong-blue', 'dark:border-gray-700');
+    emailInput.value = '';
+    thanksMessage.classList.add("flex", "items-center", "bottom-6", "text-green-500");
+    thanksMessage.classList.remove("hidden");
+    
         
        // Perform further actions (e.g., submit the form, send notification)
     // Add your code here...
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors');
+import colors from 'tailwindcss/colors';
 
-module.exports = {
-  content: [
+export const content = [
   './pages/**/*.{html,js}',
   './components/**/*.{html,js}',
   './index.html',
-	'./public/index.html', 
-	'./src/**/*.{html,js}',
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      "custom-red": "#FF5466",
-      "custom-white": "#FFFFFF",
-      "custom-gray": "#969696",
-      "custom-gray-strong": "#111827",
-      "custom-black": "#15202A",
-      "light-blue": "#C2D3FF",
-      "pale-blue": "hsl(223, 100%, 88%)",
-      "strong-blue": "#4C7BF3",
-      ...colors,
-    },
-    fontFamily: {
-      sans: ['Libre Franklin', 'sans-serif'],
-    },
-    fontSize: {
-      normal: "1.25rem",
-    },
+  './public/index.html',
+  './src/**/*.{html,js}',
+];
+
+export const darkMode = 'class';
+export const theme = {
+  extend: {},
+  colors: {
+    transparent: 'transparent',
+    current: 'currentColor',
+    "custom-red": "#FF5466",
+    "custom-white": "#FFFFFF",
+    "custom-gray": "#969696",
+    "custom-gray-strong": "#111827",
+    "custom-black": "#15202A",
+    "light-blue": "#C2D3FF",
+    "pale-blue": "hsl(223, 100%, 88%)",
+    "strong-blue": "#4C7BF3",
+    ...colors,
   },
-  plugins: [],
-}
+  fontFamily: {
+    sans: ['Libre Franklin', 'sans-serif'],
+  },
+  fontSize: {
+    normal: "1.25rem",
+  },
+};
+export const plugins = [];
